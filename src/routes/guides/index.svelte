2a19c4bb34e133fb94ae78f2c3f96fb054a1de64
catalin.festila <catafest76@gmail.com>
2022-04-18 10:29:33 +0300

    push cu cod sursa pentru web jsonplaceholder

--- src/routes/guides/index.svelte
+++ src/routes/guides/index.svelte
@@ -2,18 +2,36 @@
 // this script area is in adition of the next one script area 
 // this script run on the server to fetch the data so it can pre-render templates 
 // expose the API to use it ... 
-  export async function load() {
+  export async function load({ fetch }) {
+    const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+    const guides = await res.json();
+
+    if(res.ok) {
+      return {
+        props: {
+          guides
+        }
+      }
+    }
+
+    return {
+      status: res.status,
+      error: new Error('Could not fetch the guides')
+    }
 
   }
 </script>
 <script>
-
+  export let guides;
 </script>
 
 <div class="guides">
   <ul>
-    <li><a href="/">Guide 1</a></li>
-    <li><a href="/">Guide 2</a></li>
+    {#each guides as guide}
+      <li>
+        <a href="/">{guide.title}</a>
+      </li>
+    {/each}
   </ul>
 </div>
 
